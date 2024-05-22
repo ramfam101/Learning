bf9d65ea7edcfb349f5445668ae1cd53dd4ea73c
Jeevan Munnangi <jrmunnangi@gmail.com>
2024-05-22 10:16:13 -0400

    first commit

--- workout/app/(tabs)/index.tsx
+++ workout/app/(tabs)/index.tsx
@@ -1,70 +1,82 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+import exercises from "../../assets/data/exercises.json"
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  const exercise = exercises[0];
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    // <ParallaxScrollView
+    //   headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+    //   headerImage={
+    //     <View style={styles.imageContainer}>
+    //       <Image
+    //         source={require('@/assets/images/workoutBG.avif')}
+    //         style={styles.reactLogo}
+    //       />
+    //     </View>
+    //   }>
+    //   {/* above part sets up picture that changes size as drag happens */}
+    //   {/* below is the text appearing in the showing tab and all elements */}
+      // <ThemedView>
+      //   <ThemedText type='default' style={styles.exerciseName}>Exercise: {exercise.name}</ThemedText>
+      //   <ThemedText type='default' style={styles.exerciseStuff}>Muscle: {exercise.muscle} | {exercise.equipment}</ThemedText>
+        
+      // </ThemedView>
+    //   <ThemedView>
+    //     <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+    //     <ThemedText>
+    //       When you're ready, run{' '}
+    //       <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+    //       <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+    //       <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+    //       <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+    //     </ThemedText>
+    //   </ThemedView>
+    // </ParallaxScrollView>
+
+    <View style={styles.container}>
+      <View style={styles.exercise}>
+        <Text style={styles.exerciseName}>{exercise.name}</Text>
+        <Text style={styles.exerciseStuff}>{exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: 'gainsboro',
+    justifyContent: 'center',
+    padding:10,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  imageContainer: {
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    height: 250,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: '85%',
+    width: '100%',
     bottom: 0,
     left: 0,
     position: 'absolute',
   },
+  exercise:{
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
+  },
+  exerciseName: {
+    fontSize: 20, 
+    fontWeight: '500'
+  },
+  exerciseStuff: {
+    color: 'dimgray',
+  }
 });
